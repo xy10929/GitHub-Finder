bddd4f248871553c245be3345dd37353ea46a4a6
Zhengyu Yang <yangzy0105@gmail.com>
2023-07-09 03:56:39 -0700

    7.9

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,11 @@
+import UserResults from '../components/users/UserResults'
+
 function Home() {
-  return <h1 className='text-6xl'>Welcome</h1>
+  return (
+    <>
+      <UserResults />
+    </>
+  )
 }
 
 export default Home
